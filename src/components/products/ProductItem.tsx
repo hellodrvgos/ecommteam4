9d73bfdbef89fa98d73bc72e9d81061cbda5db46
Alina <alinbogz@gmail.com>
2023-01-16 22:16:01 +0100

    remove doesn't work

--- src/components/products/ProductItem.tsx
+++ src/components/products/ProductItem.tsx
@@ -1,17 +1,99 @@
-import { ProductDetail } from "../../types/type";
-import "./ProductItem.css"
+import React, { useState } from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import { red } from "@mui/material/colors";
+import Snackbar from "@mui/material/Snackbar";
+import { AlertTitle } from "@mui/material";
 
-export default function ProductItem({product}: ProductDetail) {
-    return (
-        <div>
-            This is Product Item
-            <p>Id: {product.id}</p>
-            <p>Name: {product.title}</p>
-            <p>Price: {product.price}</p>
-            <p>Description: {product.description}</p>
-            <p>Category: {product.caterogy}</p>
-            <img className="product_image" src={product.image} />
-            <p>Rating: {product.rating.rate}</p>
-        </div>
-    )
+import Alert from "@mui/material/Alert";
+
+import { AppDispatch, RootState } from "../../redux/store";
+
+import { ProductDetail } from "../../types/type";
+import "./ProductItem.css";
+import { Box } from "@mui/system";
+import { productActions } from "../../redux/slice/products";
+
+export default function ProductItem({ product }: ProductDetail) {
+  const dispatch = useDispatch<AppDispatch>();
+  const favList = useSelector((state: RootState) => state.products.productList);
+  const isDuplicated = favList.some((favItem) => favItem.id === product.id);
+  const isFavorite = favList.some((element) => element.id === product.id);
+
+  const [open, setOpen] = useState(false);
+  const [add, setAdd] = useState(false);
+
+  const handleAdd = () => [setAdd(true)];
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+    setAdd(false);
+  };
+
+  return (
+    <Box style={{ margin: "auto" }}>
+      <Card
+        sx={{
+          width: 300,
+          height: 500,
+          marginInline: "1rem",
+          marginBlock: "1rem",
+        }}
+      >
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image={product.image}
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {product.title}
+            </Typography>
+            <Typography>{product.caterogy}</Typography>
+            <Typography variant="body2" color="text.secondary">
+              {product.description}
+            </Typography>
+          </CardContent>
+          <Box>{product.rating.rate}</Box>
+          <FavoriteBorderIcon
+            aria-label="addFav"
+            sx={{ color: isFavorite ? red[500] : "#474444" }}
+            onClick={() => {
+              isDuplicated
+                ? handleClick()
+                : dispatch(productActions.addWish(product)) && handleAdd();
+            }}
+          />
+        </CardActionArea>
+      </Card>
+      <Snackbar open={add} autoHideDuration={1000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success">
+          <AlertTitle>Success</AlertTitle>
+          Item added to favorites
+        </Alert>
+      </Snackbar>
+
+      <Snackbar open={open} autoHideDuration={1000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
+          The country is already inside favorite list!
+        </Alert>
+      </Snackbar>
+    </Box>
+  );
 }
--- src/redux/slice/products.ts
+++ src/redux/slice/products.ts
@@ -1,24 +1,33 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 import { Product } from "../../types/type";
 
 type InitialState = {
-    productList: Product[];
-}
+  productList: Product[];
+};
 
 const initialState: InitialState = {
-    productList: [],
-}
+  productList: [],
+};
 
 const productSlice = createSlice({
-    name: "products",
-    initialState,
-    reducers: {
-        getProductData: (state, action) => {
-            state.productList = action.payload;
-        }
-     }
-})
+  name: "products",
+  initialState,
+  reducers: {
+    getProductData: (state, action) => {
+      state.productList = action.payload;
+    },
+    addWish: (state, action: PayloadAction<Product>) => {
+      state.productList.push(action.payload);
+    },
+    removeWish: (state, action) => {
+      const result = state.productList.filter(
+        (product) => product.id !== action.payload
+      );
+      state.productList = result;
+    },
+  },
+});
 
 export const productActions = productSlice.actions;
 
