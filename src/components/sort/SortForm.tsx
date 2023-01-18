0b8bbe3fb7a6ed965084dc179166995964c25d0e
Alina <alinbogz@gmail.com>
2023-01-18 11:18:26 +0100

    form changed

--- src/components/sort/SortForm.tsx
+++ src/components/sort/SortForm.tsx
@@ -6,55 +6,71 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import { productActions } from "../../redux/slice/products";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
 import { AppDispatch } from "../../redux/store";
 import { fetchProductData } from "../../redux/thunk/products";
 
 function SortForm() {
-  const [click, setClick] = useState(false);
   const dispatch = useDispatch<AppDispatch>();
   useEffect(() => {
     dispatch(fetchProductData());
   }, [dispatch]);
 
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
   const handleClose = () => {
-    setClick(true);
+    setAnchorEl(null);
   };
 
   return (
     <div>
-      <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-        <InputLabel id="demo-simple-select-standard-label">
-          Filter by
-        </InputLabel>
-        <Select
-          value={click}
-          onClick={handleClose}
-          labelId="demo-simple-select-helper-label"
-          label="Age"
+      <Button
+        id="basic-button"
+        aria-controls={open ? "basic-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+      >
+        Filter by
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem
+          onClick={() => {
+            dispatch(productActions.sortByTitle());
+            handleClose();
+          }}
         >
-          <MenuItem
-            onClick={() =>
-              dispatch(productActions.sortByTitle()) && handleClose()
-            }
-          >
-            Title
-          </MenuItem>
-          <MenuItem
-            onClick={() =>
-              dispatch(productActions.sortByPrice()) && handleClose()
-            }
-          >
-            Price
-          </MenuItem>
-          <MenuItem
-            onClick={() =>
-              dispatch(productActions.sortByCategory()) && handleClose()
-            }
-          >
-            Category
-          </MenuItem>
-        </Select>
-      </FormControl>
+          Title
+        </MenuItem>
+        <MenuItem
+          onClick={() => {
+            dispatch(productActions.sortByPrice());
+            handleClose();
+          }}
+        >
+          Price
+        </MenuItem>
+        <MenuItem
+          onClick={() => {
+            dispatch(productActions.sortByCategory());
+            handleClose();
+          }}
+        >
+          Category
+        </MenuItem>
+      </Menu>
     </div>
   );
 }
